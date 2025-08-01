c6dc79d6140fcc5fc9079077581e6e37537686e6
JeanArdila711 <jardilaa@eafit.edu.co>
2025-08-01 11:00:16 -0500

    actualizando menus

--- TallerP1/movie/models.py
+++ TallerP1/movie/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
-# Create your models here.
+class Movie(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    url = models.URLField()
+    image = models.ImageField(upload_to='movies/')
+
+class Review(models.Model):
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    date_submitted = models.DateTimeField(auto_now_add=True)
+    watch_again = models.BooleanField(default=False)
+
+class News(models.Model):
+    headline = models.CharField(max_length=200)
+    story = models.TextField()
+    date = models.DateField()
